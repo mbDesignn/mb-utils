43a1c558193ba53cb2e6808a264f5950303c78ee
Moobye <moobyes@gmail.com>
2020-08-10 15:50:22 +0800

    feat: 增加主函数封装包

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@
  * @Description: 首页
  * @Author: Moobye
  * @Date: 2020-08-07 22:48:08
- * @LastEditTime: 2020-08-09 14:16:23
+ * @LastEditTime: 2020-08-10 15:49:34
  * @LastEditors: Moobye
  */
 
 ;(function (global, factory) {
   'use strict'
-  if (typeof module === 'object' && typeof module.exports === 'object') {
-    module.exports = factory()
-  } else if (typeof define === 'function' && define.amd) {
-    define([], factory)
-  } else {
-    global.mb = factory()
-  }
-})(this, function () {
+
+  typeof module === 'object' && typeof module.exports === 'object' ? module.exports = factory() : typeof define === 'function' && define.amd ? define([], factory) : global.mb = factory()
+})(this, function (global) {
   'use strict'
 
+/**
+ * @description: 日期时间格式化方法
+ * @param  date {Date}，日期
+ * @param formatType {String}, 需要返回的日期格式，如 'yyyy-mm-dd'，或者 'yyyy-mm-dd hh:mm:ss'
+ * @return {string} 返回的格式化日期
+ * @example 使用案例，
+ */
 })
